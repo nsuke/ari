3004614e277d0970bf61a2aa5e6b27836b2b9dfd
N.Sukegawa <nsukeg@gmail.com>
2014-05-05 17:50:48 +0900

    Configure test framework

---
+++ test/karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config){
+  config.set({
+    basePath : '../',
+    files : [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+    autoWatch : true,
+    frameworks: ['jasmine'],
+    browsers: ['PhantomJS'],
+    plugins: [
+      'karma-junit-reporter',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
+    ],
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+  });
+};
---
+++ test/unit/ctrlSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('ari controllers', function() {
+  describe('AriCtrl', function() {
+    beforeEach(module('ariApp'));
+
+    it('should succeed', inject(function($controller) {
+      expect(false).toBe(false);
+    }));
+  });
+});
